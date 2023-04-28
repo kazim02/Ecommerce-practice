56da0c92e1c0cf479f6cd7e85e39d6f05f7dc2c1
kazim02 <kazim.fahm123@gmail.com>
2023-04-28 16:28:59 +0530

    changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+    <div className='container-fluid'>
+    <div className='row'>
+        kazim
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Header
